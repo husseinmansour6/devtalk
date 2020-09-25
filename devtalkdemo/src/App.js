331b7b11e26691d00e5fadb45de68da60c1d0690
husseinmansour6 <hmansour@aldogroup.com>
2020-09-25 12:33:20 -0400

    first commit

---
+++ devtalkdemo/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div className="mainDiv" data-test="main">
+      <h1 className="foobar">header</h1>
+      <p className="desc">desc</p>
+    </div>
+  )
+}
+
+export default App
