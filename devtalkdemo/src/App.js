f780885f67244ad13d2436f6504988420956eb08
husseinmansour6 <hmansour@aldogroup.com>
2020-09-25 14:11:07 -0400

    class component

--- devtalkdemo/src/App.js
+++ devtalkdemo/src/App.js
@@ -1,16 +1,30 @@
-import React from "react"
+import React, { Component } from "react"
+import Proptypes from "prop-types"
 
-const App = () => {
-  return (
-    <div className="mainDiv" data-test="mainDiv">
-      <h1 className="header" data-test="header">
-        header
-      </h1>
-      <p className="desc" data-test="desc">
-        desc
-      </p>
-    </div>
-  )
+class App extends Component {
+  render() {
+    const { header, desc } = this.props
+
+    if (!header) {
+      return null
+    }
+
+    return (
+      <div className="mainDiv" data-test="mainDiv">
+        <h1 className="header" data-test="header">
+          {header}
+        </h1>
+        <p className="desc" data-test="desc">
+          {desc}
+        </p>
+      </div>
+    )
+  }
+}
+
+App.prototypes = {
+  header: Proptypes.string,
+  desc: Proptypes.string
 }
 
 export default App
--- devtalkdemo/src/App.test.js
+++ devtalkdemo/src/App.test.js
@@ -3,21 +3,38 @@ import { shallow } from "enzyme"
 import App from "./App"
 
 describe("App component", () => {
-  let component
-  beforeEach(() => {
-    component = shallow(<App />)
-    return component
+  describe("have props", () => {
+    let component
+    beforeEach(() => {
+      const props = {
+        header: "header1",
+        desc: "desc1"
+      }
+      component = shallow(<App {...props} />)
+      return component
+    })
+    it("should render without errors", () => {
+      const wrapper = component.find("[data-test='mainDiv']")
+      expect(wrapper.length).toEqual(1)
+    })
+    it("should have header", () => {
+      const header = component.find("[data-test='header']")
+      expect(header.length).toEqual(1)
+    })
+    it("should have description", () => {
+      const desc = component.find("[data-test='desc']")
+      expect(desc.length).toEqual(1)
+    })
   })
-  it("should render without errors", () => {
-    const wrapper = component.find("[data-test='mainDiv']")
-    expect(wrapper.length).toEqual(1)
-  })
-  it("should have header", () => {
-    const header = component.find("[data-test='header']")
-    expect(header.length).toEqual(1)
-  })
-  it("should have description", () => {
-    const desc = component.find("[data-test='desc']")
-    expect(desc.length).toEqual(1)
+  describe("have no props", () => {
+    let component
+    beforeEach(() => {
+      component = shallow(<App />)
+      return component
+    })
+    it("should not render", () => {
+      const wrapper = component.find("[data-test='mainDiv']")
+      expect(wrapper.length).toEqual(0)
+    })
   })
 })
